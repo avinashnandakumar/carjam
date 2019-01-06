7d40547a87d6df6217537b7c8f5bcb6e3b0c8a58
Avinash Nandakumar <avinashnandakumar@berkeley.edu>
2019-01-05 23:53:39 -0800

    Queue UI working

--- app/src/main/java/sideprojects/carjam/MainActivity.java
+++ app/src/main/java/sideprojects/carjam/MainActivity.java
@@ -2,16 +2,20 @@ package sideprojects.carjam;
 
 import android.Manifest;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.*;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -20,15 +24,22 @@ import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.telephony.SmsMessage;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -49,6 +60,7 @@ import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.android.volley.RequestQueue;
 import com.android.volley.Request;
 
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -60,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
     private SmsReciever mReciever;
     private Integer queueLimitPeriod = 2;
     private Map<String, Integer> queueLimit;
-    //private IntentFilter mConnectionChangeIntentFilter = new IntentFilter();
-    //private SmsReceiver smsReceiver = new SmsReceiver();
+    RecyclerView recyclerView;
+    QueueListAdapter QLA;
 
     private int MY_PERMISSIONS_REQUEST_SMS_RECEIVE = 10;
 
@@ -70,8 +82,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         queueLimit = new HashMap<String,Integer>();
-        //updateMessage(" ");
-        //registerReceiver(smsReceiver, mConnectionChangeIntentFilter);
+        QLA = new QueueListAdapter();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(QLA);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
         AuthenticationRequest.Builder builder =
                 new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
         builder.setScopes(new String[]{"streaming"});
@@ -97,6 +114,77 @@ public class MainActivity extends AppCompatActivity {
         //connected();
     }
 
+    /*class Person {
+        String name;
+        String age;
+        int photoId;
+
+        Person(String name, String age, int photoId) {
+            this.name = name;
+            this.age = age;
+            this.photoId = photoId;
+        }
+    }
+
+    private List<Person> persons;
+
+    // This method creates an ArrayList that has three Person objects
+// Checkout the project associated with this tutorial on Github if
+// you want to use the same images.
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", 0));
+        persons.add(new Person("Lavery Maiss", "25 years old", 0));
+        persons.add(new Person("Lillie Watts", "35 years old", 0));
+    }
+
+    public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder>{
+
+        public class PersonViewHolder extends RecyclerView.ViewHolder {
+            CardView cv;
+            TextView personName;
+            TextView personAge;
+            ImageView personPhoto;
+
+            PersonViewHolder(View itemView) {
+                super(itemView);
+                cv = (CardView)itemView.findViewById(R.id.cv);
+                personName = (TextView)itemView.findViewById(R.id.person_name);
+                personAge = (TextView)itemView.findViewById(R.id.person_age);
+                personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+            }
+
+        }
+        List<Person> persons;
+        RVAdapter(List<Person> persons){
+            this.persons = persons;
+        }
+
+        @Override
+        public int getItemCount() {
+            return persons.size();
+        }
+
+        @Override
+        public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_main, viewGroup, false);
+            PersonViewHolder pvh = new PersonViewHolder(v);
+            return pvh;
+        }
+        @Override
+        public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+            personViewHolder.personName.setText(persons.get(i).name);
+            personViewHolder.personAge.setText(persons.get(i).age);
+            personViewHolder.personPhoto.setImageResource(persons.get(i).photoId);
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }*/
+
+
+    /*}*/
     private void connected() {
         // Play a playlist
         //SpotifyAppRemote.connect(this, mConnectionParams, mConnectionListener);
@@ -104,7 +192,13 @@ public class MainActivity extends AppCompatActivity {
         final TextView songName = (TextView) findViewById(R.id.currentSongName);
         final TextView songArtist = (TextView) findViewById(R.id.currentSongArtist);
         final ImageView songImage = (ImageView) findViewById(R.id.currentSongImage);
-
+        //final RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+        /*initializeData();
+        rv.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);*/
         mSpotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
                 .setEventCallback(playerState -> {
@@ -143,6 +237,9 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.RECEIVE_SMS},
                 MY_PERMISSIONS_REQUEST_SMS_RECEIVE);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.SEND_SMS},
+                24);
     }
 
     @Override
@@ -257,7 +354,9 @@ public class MainActivity extends AppCompatActivity {
             //System.out.println(fullMessage);
             Pattern p2 = Pattern.compile("a song for you");
             Matcher matcher2 = p2.matcher(fullMessage);
-            if(matcher2.find()){
+            Pattern p3 = Pattern.compile("https://open.spotify.com");
+            Matcher matcher3 = p3.matcher(fullMessage);
+            if(matcher2.find() || matcher3.find()){
                 //String temp = messageBody.substring(6, 15);
                 //if (temp.equals(" a song f")) {
                 System.out.println("In the loop");
@@ -278,22 +377,45 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void deleteText(Context context, String sender, String message){
         Uri inbox = Uri.parse("content://sms/inbox");
         String[] columns = new String[] { "_id","address", "body" };
-        Cursor c = context.getContentResolver().query(inbox, columns, null,
+        Cursor c = context.getContentResolver().query(inbox, new String[] { "_id", "thread_id", "address",
+                        "person", "date", "body" }, null,
                 null, null);
         boolean MatchFound=false;
         c.moveToNext();
-        while(!MatchFound){
-            Integer index = c.getColumnIndexOrThrow("body");
-            String dataMessage = c.getString(index);
-            System.out.println(dataMessage);
-            if(dataMessage.equals(message)){
-                System.out.println("Found a match");
-                MatchFound=true;
+        try {
+            while (!MatchFound) {
+            /*Integer index = c.getColumnIndexOrThrow("body");
+            Integer pidIndex = c.getColumnIndexOrThrow("_id");
+            String pid = c.getString(pidIndex);
+            String dataMessage = c.getString(index);*/
+                String id = c.getString(0);
+                long threadId = c.getLong(1);
+                String address = c.getString(2);
+                String body = c.getString(5);
+                System.out.println(body);
+                if (body.equals(message)) {
+                    System.out.println("Found a match");
+                    ContentValues cv = new ContentValues();
+                    cv.put(Telephony.Sms.ADDRESS, address);
+                    cv.put(Telephony.Sms.BODY, body);
+                    cv.put(Telephony.Sms.TYPE, Telephony.Sms.MESSAGE_TYPE_INBOX);
+                    //Uri uri = getContentResolver().insert(Telephony.Sms.CONTENT_URI, cv);
+                    MatchFound = true;
+                    //String uri = Telephony.Sms.CONTENT_URI.buildUpon().appendPath(id);
+                    int deleted = context.getContentResolver().delete(Uri.parse("content://sms/"), "_id=? and thread_id=?",new String[] { String.valueOf(id),String.valueOf(threadId) });//"_id=?", new String[]{String.valueOf(id)});
+                    System.out.println("Deleting SMS with id: " + threadId);
+                    System.out.println(Uri.parse("content://sms/"+id).toString());
+                    System.out.println("_id" + id);
+                    System.out.println(deleted);
+                }
+                c.moveToNext();
             }
-            c.moveToNext();
+        }catch(Exception e){
+            System.out.println("did not delete the messssage");
         }
     }
     private void addToQueue(String sender, String songId) {
@@ -317,5 +439,62 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void skipTrack(View view){
+        mSpotifyAppRemote.getPlayerApi().skipPrevious();
+    }
+
+    public void backTrack(View view){
+        mSpotifyAppRemote.getPlayerApi().skipNext();
+
+    }
+
+    public void play(View view){
+
+        mSpotifyAppRemote.getPlayerApi().getPlayerState().setResultCallback(playerState -> {
+            if(playerState.isPaused){
+                mSpotifyAppRemote.getPlayerApi().resume();
+            }
+            else{
+                mSpotifyAppRemote.getPlayerApi().pause();
+            }
+        });
+    }
+
+    public void saveTrack(View view){
+        mSpotifyAppRemote.getPlayerApi().getPlayerState().setResultCallback(playerState -> {
+          mSpotifyAppRemote.getUserApi().addToLibrary(playerState.track.uri);
+        });
+    }
+
+    public class QueueListAdapter extends RecyclerView.Adapter<QueueListAdapter.ViewHolder>{
+        String[] fakeData = new String[]{"Sicko Mode", "God's Plan", "Nonstop", "HYFR", "Moonlight", "SAD!"};
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int i){
+            //Create a new view
+            CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_task, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder vH, int i){
+            vH.titleView.setText(fakeData[i]);
+        }
+
+        @Override
+        public int getItemCount(){
+            return fakeData.length;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            CardView cardView;
+            TextView titleView;
+
+            public ViewHolder(CardView card){
+                super(card);
+                cardView = card;
+                titleView = (TextView) card.findViewById(R.id.text1);
+            }
+        }
+    }
 
 }
